8772cbd93c1e774f682720e1ed418abd8eda5408
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-07-07 20:50:52 +0530

    Added: method to check person by city

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -5,16 +5,12 @@ import java.util.Scanner;
 public class AddressBookMain {
 
 
-    public static void main(String[] args) {
-        AddressBookMain addressBookMain = new AddressBookMain();
-
-        addressBookMain.choose();
-    }
-
     public void choose() {
         MultipleAddressBook obj = new MultipleAddressBook();
         while (true) {
-            System.out.println("Enter \n 1. To add The new AddressBook\n 2. To add contact in AddressBook\n " + "3. To edit the contact in AddressBook\n 4. To delete the contact in AddressBook\n 5. To delete the AddressBook\n " + "6. To Print the AddressBook\n 7. To Print the contacts in AddressBook\n 0. to exit");
+            System.out.println("Enter \n 1. To add The new AddressBook\n 2. To add contact in AddressBook\n "
+                    + "3. To edit the contact in AddressBook\n 4. To delete the contact in AddressBook\n 5. To delete the AddressBook\n "
+                    + "6. To Print the AddressBook\n 7. To Print the contacts in AddressBook\n 0. to exit");
             Scanner scanner = new Scanner(System.in);
             int choice = scanner.nextInt();
             switch (choice) {
@@ -46,5 +42,12 @@ public class AddressBookMain {
                     System.out.println("Enter the wrong input");
             }
         }
+    }
+
+
+    public static void main(String[] args) {
+        AddressBookMain addressBookMain = new AddressBookMain();
+
+        addressBookMain.choose();
     }
 }
